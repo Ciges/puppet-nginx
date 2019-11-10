bbe2760a9e724346f7cdce180c2f3dc3355c7653
Ciges <jmanuel@ciges.net>
2019-11-10 15:42:03 +0100

    Changed port from 8080 to 8090 for ciges.net proxy

--- examples/test/manifests/cigesnet.pp
+++ examples/test/manifests/cigesnet.pp
@@ -4,6 +4,6 @@ include nginx
 # Requests are sent to http://www.ciges.net at 164.132.103.253
 nginx::resource::server { 'ciges.net':
     ensure => present,
-    listen_port => 8080,
+    listen_port => 8090,
     proxy => 'http://164.132.103.253/',
 } 
