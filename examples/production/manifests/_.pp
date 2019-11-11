67d194368fd4f0d92abe61677ced32f9b4ba1c41
Ciges <jmanuel@ciges.net>
2019-11-11 01:18:04 +0100

    Corriged time unit to seconds in request_time

--- examples/production/manifests/_.pp
+++ examples/production/manifests/_.pp
@@ -1,7 +1,7 @@
 class { 'nginx':
   log_format => {
-    proxy_log => '[$time_local] $remote_addr - "$request" $status - $request_time msec',
-    upstream_log =>  '[$time_local] $remote_addr - $server_name  to: $upstream_addr: $request - upstream_response_time $upstream_response_time msec $msec request_time $request_time msec',
+    proxy_log => '[$time_local] $remote_addr - "$request" $status - $request_time sec',
+    upstream_log =>  '[$time_local] $remote_addr - $server_name  to: $upstream_addr: $request - upstream_response_time $upstream_response_time msec $msec request_time $request_time sec',
   }
 }
 
